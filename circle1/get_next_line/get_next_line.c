9cd8a9f00ece21c869704fd335e6f6de43572b96
Dagyo Kang <dakang@c2r7s4.42seoul.kr>
2023-11-10 20:15:11 +0900

    test

--- circle1/get_next_line/get_next_line.c
+++ circle1/get_next_line/get_next_line.c
@@ -3,99 +3,101 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dakyo <dakyo@student.42.fr>                +#+  +:+       +#+        */
+/*   By: dakang <dakang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/09 21:48:11 by dakyo             #+#    #+#             */
-/*   Updated: 2023/11/09 22:03:15 by dakyo            ###   ########.fr       */
+/*   Created: 2023/11/09 21:48:11 by dakang            #+#    #+#             */
+/*   Updated: 2023/11/10 20:14:08 by dakang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int find_new_line(char *total_line)
+int	find_new_line(char *total_line)
 {
-    int line_len;
-    int i;
+	int	line_len;
+	int	i;
 
-    line_len = 0;
-    i = -1;
-    while (total_line[line_len])
-        line_len++;
-    while (++i < line_len)
-    {
-        if (total_line[i] == '\n')
-            return (i);
-    }
-    return (-1);
+	line_len = 0;
+	i = -1;
+	while (total_line[line_len])
+		line_len++;
+	while (++i < line_len)
+	{
+		if (total_line[i] == '\n')
+			return (i);
+	}
+	return (-1);
 }
 
-char *check_new_line(char **total_line, char *buffer)
+char	*check_new_line(char **total, char *buffer)
 {
-    char *result;
-    char *temp;
-    int new_line_len;
+	char	*result;
+	char	*temp;
+	int		new_line_len;
 
-    result = NULL;
-    if (find_new_line(*total_line) == -1)
-    {
-        if (*total_line[0] != '\0')
-            result = ft_strdup(*total_line, ft_strlen(*total_line));
-        free(*total_line);
-        *total_line = NULL;
-    }
-    else
-    {
-        result = ft_strdup(*total_line, find_new_line(*total_line) + 1);
-        new_line_len = ft_strlen(*total_line + find_new_line(*total_line) + 1);
-        temp = ft_strdup((*total_line + find_new_line(*total_line) + 1), new_line_len);
-        free(*total_line);
-        *total_line = temp;
-    }
-    free(buffer);
-    return (result);
+	result = NULL;
+	if (find_new_line(*total) == -1)
+	{
+		if (*total[0] != '\0')
+			result = ft_strdup(*total, ft_strlen(*total));
+		free(*total);
+		*total = NULL;
+	}
+	else
+	{
+		result = ft_strdup(*total, find_new_line(*total) + 1);
+		new_line_len = ft_strlen(*total + find_new_line(*total) + 1);
+		temp = ft_strdup((*total + find_new_line(*total) + 1), new_line_len);
+		free(*total);
+		*total = temp;
+	}
+	free(buffer);
+	return (result);
 }
 
-char *read_buffer(int fd, char *buffer, char **total_line)
+char	*read_buffer(int fd, char *buffer, char **total_line)
 {
-    ssize_t read_size;
-    char *temp;
+	ssize_t	read_size;
+	char	*temp;
 
-    buffer[BUFFER_SIZE] = '\0';
-    read_size = read(fd, buffer, BUFFER_SIZE);
-    while (read_size > 0)
-    {
-        buffer[read_size] = '\0';
-        temp = ft_strjoin(*total_line, buffer);
-        free(*total_line);
-        *total_line = temp;
-        if (find_new_line(*total_line) != -1)
-            return (check_new_line(total_line, buffer));
-        read_size = read(fd, buffer, BUFFER_SIZE);
-    }
-    return (check_new_line(total_line, buffer));
+	buffer[BUFFER_SIZE] = '\0';
+	read_size = read(fd, buffer, BUFFER_SIZE);
+	while (read_size > 0)
+	{
+		buffer[read_size] = '\0';
+		temp = ft_strjoin(*total_line, buffer);
+		free(*total_line);
+		*total_line = temp;
+		if (find_new_line(*total_line) != -1)
+			return (check_new_line(total_line, buffer));
+		read_size = read(fd, buffer, BUFFER_SIZE);
+	}
+	return (check_new_line(total_line, buffer));
 }
 
 char	*get_next_line(int fd)
 {
-    static char *total_line;
-    char *buffer;
+	static char	*total_line;
+	char		*buffer;
 
-    buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    if (buffer == NULL)
-        return (NULL);
-    if (BUFFER_SIZE < 0 || read(fd, buffer, BUFFER_SIZE) == -1)
-    {
-        free(buffer);
-        return (NULL);
-    }
-    if (total_line != NULL && find_new_line(total_line) != -1) // 쌓인 데이터 안에 개행이 있는 경우
-        return (check_new_line(&total_line, buffer));
-    if (total_line == NULL) // 첫번째 호출 or 이전 호출에서 읽은 데이터가 없는 경우
-    {
-        total_line = (char *)malloc(sizeof(char) * 1);
-        if (total_line == NULL)
-            return (NULL);
-        total_line[0] = '\0';
-    }
-    return (read_buffer(fd, buffer, &total_line));
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (buffer == NULL)
+		return (NULL);
+	if (BUFFER_SIZE < 0 || read(fd, buffer, 0) == -1)
+	{
+		free(buffer);
+		free(total_line);
+		total_line = NULL;
+		return (NULL);
+	}
+	if (total_line != NULL && find_new_line(total_line) != -1)
+		return (check_new_line(&total_line, buffer));
+	if (total_line == NULL)
+	{
+		total_line = (char *)malloc(sizeof(char) * 1);
+		if (total_line == NULL)
+			return (NULL);
+		total_line[0] = '\0';
+	}
+	return (read_buffer(fd, buffer, &total_line));
 }
--- circle1/get_next_line/get_next_line.h
+++ circle1/get_next_line/get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dakyo <dakyo@student.42.fr>                +#+  +:+       +#+        */
+/*   By: dakang <dakang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 21:43:11 by dakang            #+#    #+#             */
-/*   Updated: 2023/11/09 21:52:14 by dakyo            ###   ########.fr       */
+/*   Updated: 2023/11/10 19:24:16 by dakang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 500
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 500
+# endif
 
-int find_new_line(char *total_line);
-char *check_new_line(char **total_line, char *buffer);
-char *read_buffer(int fd, char *buffer, char **total_line);
-char *get_next_line(int fd);
+int		find_new_line(char *total_line);
+char	*check_new_line(char **total_line, char *buffer);
+char	*read_buffer(int fd, char *buffer, char **total_line);
+char	*get_next_line(int fd);
 
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *s1, int size);
