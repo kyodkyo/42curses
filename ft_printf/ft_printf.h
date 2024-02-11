61b652c0015da89191f34a96470830dfe7421da9
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-02-12 02:03:43 +0900

    헤더 파일 메서드 추가

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,15 +6,20 @@
 /*   By: dakyo <dakyo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 14:19:08 by dakyo             #+#    #+#             */
-/*   Updated: 2024/01/30 14:19:57 by dakyo            ###   ########.fr       */
+/*   Updated: 2024/02/12 01:58:01 by dakyo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdarg.h>
-#include <unistd.h>
+# include <stdarg.h>
+# include <unistd.h>
 
+int	ft_print_c(va_list *v_list);
+int	ft_print_s(va_list *v_list);
+int	ft_prinf_p(va_list *v_list);
+int	ft_specifier(va_list *v_list, const char c);
+int	ft_printf(char const *str, ...);
 
 #endif
