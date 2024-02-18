489e489c46f9dc35177921fcf72b30dc35672b01
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-02-18 22:04:56 +0900

    feat: stack free method

--- push_swap/etc.c
+++ push_swap/etc.c
@@ -6,7 +6,7 @@
 /*   By: dakyo <dakyo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 19:56:07 by dakyo             #+#    #+#             */
-/*   Updated: 2024/02/18 20:07:48 by dakyo            ###   ########.fr       */
+/*   Updated: 2024/02/18 21:17:54 by dakyo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,19 @@ void	free_arr(int argc, char **arr, char *res)
 	}
 	free(arr);
 	free(res);
+}
+
+void	free_stack(t_stack *stack)
+{
+	t_node	*temp;
+	t_node	*node;
+
+	node = stack -> top;
+	while (node)
+	{
+		temp = node -> next;
+		free(node);
+		node = NULL;
+		node = temp;
+	}
 }
