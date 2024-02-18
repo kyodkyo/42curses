c28b20d33a5aa4e13ef4a098a482f2fbb2324ec8
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-02-18 22:05:23 +0900

    feat: atoi, lstnew method add

--- push_swap/ft_utils.c
+++ push_swap/ft_utils.c
@@ -6,7 +6,7 @@
 /*   By: dakyo <dakyo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 14:53:31 by dakyo             #+#    #+#             */
-/*   Updated: 2024/02/18 16:33:22 by dakyo            ###   ########.fr       */
+/*   Updated: 2024/02/18 21:09:36 by dakyo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,42 @@ int	ft_strlen(const char *str, int k)
 		i++;
 	}
 	return (i);
+}
+
+int	ft_atoi(const char *str)
+{
+	int		i;
+	int		flag;
+	long	num;
+
+	i = 0;
+	flag = 1;
+	num = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			flag = -1;
+		i++;
+	}
+	while ('0' <= str[i] && str[i] <= '9')
+	{
+		num = num * 10 + str[i] - '0';
+		i++;
+	}
+	return ((int)num * flag);
+}
+
+t_node	*ft_lstnew(int content)
+{
+	t_node	*node;
+
+	node = (t_node *)malloc(sizeof(t_node));
+	if (!node)
+		return (NULL);
+	node -> content = content;
+	node -> prev = NULL;
+	node -> next = NULL;
+	return (node);
 }
