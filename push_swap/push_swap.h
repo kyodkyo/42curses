abeebb11a7ece522a96e3f9394b58c89c63d0d37
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-02-18 22:06:55 +0900

    feat: header update

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dakyo <dakyo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 16:35:42 by dakyo             #+#    #+#             */
-/*   Updated: 2024/02/18 20:31:00 by dakyo            ###   ########.fr       */
+/*   Updated: 2024/02/18 21:09:48 by dakyo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ typedef struct s_node
 typedef struct s_stack
 {
 	int				size;
-	int				flag;
+	char			name;
 	struct s_node	*top;
 	struct s_node	*bottom;
 }	t_stack;
@@ -38,11 +38,17 @@ void	free_arr(int argc, char **arr, char *res);
 
 /** ft_utils.c */
 int		ft_strlen(const char *str, int k);
+int		ft_atoi(const char *str);
+t_node	*ft_lstnew(int content);
 
 /** input_preprocess.c */
 int		num_count_in_argv(char *argv);
 char	**split_input(int argc, char **argv, int size);
 char	*split_in_argv(int argc, char *argv, int *k, char **arr);
 int		check_valid(char *str);
+
+/** set_stack.c */
+t_stack	*make_stack(int size, char name);
+void	init_push_stack_a(int argc, char **arr, t_stack *stack_a);
 
 #endif
