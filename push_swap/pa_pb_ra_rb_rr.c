a1025d6e3eed01016cb698caafc673239e584e57
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-03-28 00:21:35 +0900

    update: file merge

--- push_swap/pa_pb_ra_rb_rr.c
+++ push_swap/pa_pb_ra_rb_rr.c
@@ -1,16 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ra_rb_rr.c                                         :+:      :+:    :+:   */
+/*   pa_pb.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dakyo <dakyo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/03 21:21:26 by dakyo             #+#    #+#             */
-/*   Updated: 2024/03/08 16:49:14 by dakyo            ###   ########.fr       */
+/*   Created: 2024/03/07 21:35:45 by dakyo             #+#    #+#             */
+/*   Updated: 2024/03/28 00:02:55 by dakyo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	pa(t_stack **stack_a, t_stack **stack_b)
+{
+	t_node	*temp;
+
+	if (!(*stack_b)->top)
+		return ;
+	temp = (*stack_b)->top;
+	(*stack_b)->top = (*stack_b)->top->next;
+	temp->next = (*stack_a)->top;
+	(*stack_a)->top = temp;
+	write(1, "pa\n", 3);
+}
+
+void	pb(t_stack **stack_a, t_stack **stack_b)
+{
+	t_node	*temp;
+
+	if (!(*stack_a)->top)
+		return ;
+	temp = (*stack_a)->top;
+	(*stack_a)->top = (*stack_a)->top->next;
+	temp->next = (*stack_b)->top;
+	(*stack_b)->top = temp;
+	write(1, "pb\n", 3);
+}
 
 void	ra(t_stack **stack_a)
 {
