c0b48e34b40f2b7b3a9e356be1afd074631572d5
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-03-30 23:30:04 +0900

    add: 3개 이하의 노드 하드코딩

--- push_swap/sorting_hard.c
+++ push_swap/sorting_hard.c
@@ -5,104 +5,142 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dakyo <dakyo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/07 21:35:06 by dakyo             #+#    #+#             */
-/*   Updated: 2024/03/07 23:16:45 by dakyo            ###   ########.fr       */
+/*   Created: 2024/03/30 21:58:53 by dakyo             #+#    #+#             */
+/*   Updated: 2024/03/30 22:57:17 by dakyo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	get_min_content(t_stack **stack_a)
+void	hard_coding(t_stack **stack_a, t_stack **stack_b, int size, int flag)
 {
-	int		min;
-	t_node	*temp;
-
-	temp = (*stack_a)->top;
-	min = temp->content;
-	while (temp->next != NULL)
+	if (flag == 1)
 	{
-		temp = temp->next;
-		if (temp->content < min)
-			min = temp->content;
-	}
-	return (min);
-}
-
-void	hard_coding(t_stack **stack_a, t_stack **stack_b)
-{
-	if ((*stack_a)->size == 1)
+		if (size == count_stack_size(stack_a))
+			three_node_a_sort(stack_a, size);
+		else
+			under_three_a_sort(stack_a, size);
 		return ;
-	else if ((*stack_a)->size == 2)
-		sa(stack_a);
-	else if ((*stack_a)->size == 3)
-		node_3_sorting(stack_a);
-	else if ((*stack_a)->size == 4)
-		node_4_sorting(stack_a, stack_b);
-	else
-		node_5_sorting(stack_a, stack_b);
+	}
+	else if (flag == 2)
+	{
+		if (size == count_stack_size(stack_b))
+		{
+			three_node_b_sort(stack_b, size);
+			while (size-- > 0)
+				pa(stack_a, stack_b);
+		}
+		else
+		{
+			under_three_b_sort(stack_b, size);
+			while (size-- > 0)
+				pa(stack_a, stack_b);
+		}
+	}
 }
 
-void	node_3_sorting(t_stack **stack_a)
+void	three_node_a_sort(t_stack **stack_a, int size)
 {
-	int	top;
-	int	middle;
-	int	bottom;
+	int	a;
+	int	b;
+	int	c;
 
-	top = (*stack_a)->top->content;
-	middle = (*stack_a)->top->next->content;
-	bottom = (*stack_a)->bottom->content;
-	while (!(top < middle && middle < bottom))
+	if (is_stack_sort(*stack_a, size, 1))
+		return ;
+	while (!(is_stack_sort(*stack_a, size, 1)))
 	{
-		if (top > middle && top > bottom)
-			ra(stack_a);
-		else if (top > middle)
+		a = (*stack_a)->top->content;
+		b = (*stack_a)->top->next->content;
+		if (size == 3)
+			c = (*stack_a)->top->next->next->content;
+		if (size == 2 || (a < b && b > c && a < c) || (a > b && b < c && a < c)
+			|| (a > b && b > c && a > c))
+		{
 			sa(stack_a);
-		else if (middle > bottom)
+			continue ;
+		}
+		if ((a > b && b < c))
+			ra(stack_a);
+		else
 			rra(stack_a);
-		top = (*stack_a)->top->content;
-		middle = (*stack_a)->top->next->content;
-		bottom = (*stack_a)->bottom->content;
 	}
 }
 
-void	node_4_sorting(t_stack **stack_a, t_stack **stack_b)
+void	under_three_a_sort(t_stack **stack_a, int size)
 {
-	int	min;
+	int	a;
+	int	b;
+	int	c;
 
-	min = get_min_content(stack_a);
-	while (1)
+	if (size == 1 || is_stack_sort(*stack_a, size, 1))
+		return ;
+	while (!(is_stack_sort(*stack_a, size, 1)))
 	{
-		if ((*stack_a)->top->content == min)
+		a = (*stack_a)->top->content;
+		b = (*stack_a)->top->next->content;
+		if (size == 3)
+			c = (*stack_a)->top->next->next->content;
+		if (size == 2 || (a > b && a > c) || (c > a && c > b))
+			sa(stack_a);
+		else if ((b > a && b > c))
 		{
-			pb(stack_a, stack_b);
-			break ;
+			ra(stack_a);
+			sa(stack_a);
+			rra(stack_a);
 		}
-		ra(stack_a);
 	}
-	node_3_sorting(stack_a);
-	pa(stack_a, stack_b);
 }
 
-void	node_5_sorting(t_stack **stack_a, t_stack **stack_b)
+void	three_node_b_sort(t_stack **stack_b, int size)
 {
-	int		i;
-	int		min;
+	int	a;
+	int	b;
+	int	c;
 
-	i = 0;
-	min = get_min_content(stack_a);
-	while (i < 2)
+	if (size == 1 || is_stack_sort(*stack_b, size, 2))
+		return ;
+	while (!(is_stack_sort(*stack_b, size, 2)))
 	{
-		if ((*stack_a)->top->content == min)
+		a = (*stack_b)->top->content;
+		b = (*stack_b)->top->next->content;
+		if (size == 3)
+			c = (*stack_b)->top->next->next->content;
+		if (size == 2 || (a > b && b < c && a > c) || (a < b && b > c && a > c)
+			|| (a < b && b < c && a < c))
 		{
-			pb(stack_a, stack_b);
-			min = get_min_content(stack_a);
-			i++;
+			sb(stack_b);
+			continue ;
 		}
-		ra(stack_a);
+		if (a < b && b > c)
+			rb(stack_b);
+		else
+			rrb(stack_b);
 	}
-	node_3_sorting(stack_a);
-	pa(stack_a, stack_b);
-	pa(stack_a, stack_b);
-	if ((*stack_a)->top->content > (*stack_a)->top->next->content)
-		sa(stack_a);
+}
+
+void	under_three_b_sort(t_stack **stack_b, int size)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	if (is_stack_sort(*stack_b, size, 2))
+		return ;
+	while (!(is_stack_sort(*stack_b, size, 2)))
+	{
+		a = (*stack_b)->top->content;
+		b = (*stack_b)->top->next->content;
+		if (size == 3)
+			c = (*stack_b)->top->next->next->content;
+		if (size == 2 || (a < b && a < c) || (c < a && c < b))
+		{
+			sb(stack_b);
+		}
+		else if ((b < a && b < c))
+		{
+			rb(stack_b);
+			sb(stack_b);
+			rrb(stack_b);
+		}
+	}
 }
