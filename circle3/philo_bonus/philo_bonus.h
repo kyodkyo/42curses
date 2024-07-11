3afe5af383156397390261ee454d9b49b27bc427
Dagyo Kang <dakang@c4r4s3.42seoul.kr>
2024-07-11 17:49:07 +0900

    add: bonus main 추가

--- circle3/philo_bonus/philo_bonus.h
+++ circle3/philo_bonus/philo_bonus.h
@@ -6,11 +6,37 @@
 /*   By: dakang <dakang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 16:36:58 by dakang            #+#    #+#             */
-/*   Updated: 2024/07/10 16:37:20 by dakang           ###   ########.fr       */
+/*   Updated: 2024/07/11 17:29:18 by dakang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_BONUS_H
 # define PHILO_BONUS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <semaphore.h>
+# include <sys/time.h>
+
+typedef struct philo{
+	int				num_of_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				number_of_must_eat;
+	int				id;
+	int				eat_count;
+	long long		last_eat_time;
+	sem_t			*forks;
+	sem_t			*print_lock;
+	sem_t			*die_lock;
+	pthread_t		thread;
+	struct timeval	time;
+}	t_philo;
+
+int	error(char *str);
 
 #endif
--- circle3/philo_bonus/utils_bonus.c
+++ circle3/philo_bonus/utils_bonus.c
@@ -1,27 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo_bonus.c                                      :+:      :+:    :+:   */
+/*   utils_bonus.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dakang <dakang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/10 16:36:21 by dakang            #+#    #+#             */
-/*   Updated: 2024/07/11 17:28:19 by dakang           ###   ########.fr       */
+/*   Created: 2024/07/11 17:28:47 by dakang            #+#    #+#             */
+/*   Updated: 2024/07/11 17:29:07 by dakang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 
-int	main(int argc, char **argv)
+int	error(char *str)
 {
-	t_philo	*philo;
-
-	if (argc != 5 && argc != 6)
-		return (error("argc error"));
-	memset(philo, 0, sizeof(t_philo));
-	if (init_philo(&philo))
-		return (error("init philo error"));
-	if (run_philo(philo))
-		return (error("philo execute error"));
-	return (0);
+	printf("%s\n", str);
+	return (1);
 }
