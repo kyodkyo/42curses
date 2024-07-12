7200ad13c50244e37186342c23f73f00510629cf
Dagyo Kang <dakang@c3r4s5.42seoul.kr>
2024-07-12 14:20:05 +0900

    bonus: info 구조체 변경

--- circle3/philo_bonus/philo_bonus.c
+++ circle3/philo_bonus/philo_bonus.c
@@ -6,22 +6,26 @@
 /*   By: dakang <dakang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 16:36:21 by dakang            #+#    #+#             */
-/*   Updated: 2024/07/11 17:28:19 by dakang           ###   ########.fr       */
+/*   Updated: 2024/07/12 14:19:33 by dakang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 
+int	init_info(int argc, char **argv, t_info *info)
+{
+}
+
 int	main(int argc, char **argv)
 {
-	t_philo	*philo;
+	t_info	*info;
 
 	if (argc != 5 && argc != 6)
 		return (error("argc error"));
-	memset(philo, 0, sizeof(t_philo));
-	if (init_philo(&philo))
-		return (error("init philo error"));
-	if (run_philo(philo))
+	memset(info, 0, sizeof(t_info));
+	if (init_info(argc, argv, &info))
+		return (error("init info error"));
+	if (run_philo(&info))
 		return (error("philo execute error"));
 	return (0);
 }
--- circle3/philo_bonus/philo_bonus.h
+++ circle3/philo_bonus/philo_bonus.h
@@ -6,7 +6,7 @@
 /*   By: dakang <dakang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 16:36:58 by dakang            #+#    #+#             */
-/*   Updated: 2024/07/11 17:29:18 by dakang           ###   ########.fr       */
+/*   Updated: 2024/07/12 14:13:08 by dakang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,25 @@
 # include <semaphore.h>
 # include <sys/time.h>
 
-typedef struct philo{
+typedef struct info{
 	int				num_of_philo;
 	int				time_to_die;
 	int				time_to_eat;
 	int				time_to_sleep;
 	int				number_of_must_eat;
+	int				finish_flag;
+	int				finished_eat_philo;
 	int				id;
 	int				eat_count;
+	long long		start_time;
 	long long		last_eat_time;
+	pid_t			pid;
 	sem_t			*forks;
 	sem_t			*print_lock;
 	sem_t			*die_lock;
-	pthread_t		thread;
+	pthread_t		monitor;
 	struct timeval	time;
-}	t_philo;
+}	t_info;
 
 int	error(char *str);
 
