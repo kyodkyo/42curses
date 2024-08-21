74a50263b959c7548f1dbdeb46bd2a3fcb342469
Dagyo Kang <dakang@cx1r1s2.42seoul.kr>
2024-08-21 20:58:10 +0900

    cpp module 00 : ex00

---
+++ circle4/cpp 00~04/cpp00/ex00/megaphone.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char **argv){
+    if (argc >= 2){
+        for(int i=1; i<argc; i++){
+            for(int j=0; argv[i][j]!='\0'; j++)
+                std::cout << (char)std::toupper(argv[i][j]);
+        }
+    }
+    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    std::cout << std::endl;
+    return 0;
+}
