87c254891474d79a23b8c061d0617f86606efce6
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-09-05 15:53:33 +0900

    cpp00: ex01 finish

---
+++ circle4/cpp 00~04/cpp00/ex01/main.cpp
@@ -0,0 +1,24 @@
+#include "phonebook.hpp"
+
+int main()
+{
+    PhoneBook ph;
+    std::string cmd;
+
+    while(true)
+    {
+        std::cout <<  "please enter command: [ADD, SEARCH, EXIT]" << std::endl;
+        std::getline(std::cin, cmd);
+        if (std::cin.eof())
+            break;
+        if (cmd == "ADD")
+            ph.Add();
+        else if (cmd == "SEARCH")
+            ph.Search();
+        else if (cmd == "EXIT")
+            break;
+        else
+            std::cout << std::endl << "Wrong command!" << std::endl;
+    }
+    return (0);
+}
--- circle4/cpp 00~04/cpp00/ex01/phonebook.cpp
+++ circle4/cpp 00~04/cpp00/ex01/phonebook.cpp
@@ -17,26 +17,49 @@ void PhoneBook::Search(void)
 {
     if (this->size)
     {
-        std::cout << std::endl << "-----------------------------------" << std::endl;
+        std::cout << std::endl << "--------------------------------------------------------------" << std::endl;
         std::cout << "|" << std::setw(10) << "index"
                 << "|" << std::setw(10) << "first name"
                 << "|" << std::setw(10) << "last name"
                 << "|" << std::setw(10) << "nick name"
                 << "|" << std::endl;
-        std::cout << std::endl << "-----------------------------------" << std::endl;
+        std::cout << std::endl << "--------------------------------------------------------------" << std::endl;
         
         for(int i=0; i<this->size; i++)
         {
             std::string first_name = this->contacts[i].getFirstName();
             std::string last_name = this->contacts[i].getLastName();
             std::string nick_name = this->contacts[i].getNickName();
+            std::cout << "|" << std::setw(10) << i
+                    << "|" << std::setw(10) << GetTenStr(first_name)
+                    << "|" << std::setw(10) << GetTenStr(last_name)
+                    << "|" << std::setw(10) << GetTenStr(nick_name)
+                    << "|" << std::endl;
         }
+        std::cout << std::endl << "--------------------------------------------------------------" << std::endl;
+
+        std::string index;
+        std::cout << std::endl << "pleasr enter index: ";
+        std::getline(std::cin, index);
+        if (std::cin.eof())
+            exit(0);
+        if (index.size() != 1 || index[0] >= '0' + size || index[0] < '0')
+            std::cout << std::endl << "Wrong index!" << std::endl;
+        else
+            PrintContact(index[0] - '0');
     }
+    else
+        std::cout << std::endl << "PhoneBook is empty" << std::endl;
 }
 
 void PhoneBook::PrintContact(int index)
 {
-
+    std::cout << std::endl << "--------------------------------------------------------------" << std::endl;
+    std::cout << "first name: " << this->contacts[index].getFirstName() << std::endl;
+    std::cout << "last name: " << this->contacts[index].getLastName() << std::endl;
+    std::cout << "nick name: " << this->contacts[index].getNickName() << std::endl;
+    std::cout << "phone number: " << this->contacts[index].getPhoneNum() << std::endl;
+    std::cout << std::endl << "--------------------------------------------------------------" << std::endl;
 }
 
 std::string PhoneBook::GetTenStr(std::string str)
