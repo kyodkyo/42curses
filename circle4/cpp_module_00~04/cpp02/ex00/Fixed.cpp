c6da8d431641a8484e4dca2265818e08991629a2
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-11-07 18:34:01 +0900

    add: ex00

--- circle4/cpp_module_00~04/cpp02/ex00/Fixed.cpp
+++ circle4/cpp_module_00~04/cpp02/ex00/Fixed.cpp
@@ -1,6 +1,31 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed() : value(0) {
-    
+Fixed::Fixed() : value(0) { 
+    std::cout << "Default constructor called" << std::endl;
 }
 
+Fixed::Fixed(const Fixed& fixed) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = fixed;
+}
+
+Fixed& Fixed::operator = (const Fixed& other) {
+    std::cout << "Copy assignment operator called" << std::endl;
+
+    if (this != &other)
+        this->value = other.getRawBits();
+
+    return *this;
+}
+
+Fixed::~Fixed(){
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const {
+    return this->value;
+}
+
+void Fixed::setRawBits(int const raw){
+    this->value = raw;
+}
