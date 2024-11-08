7632c4220531c82f0235d69a8e84af4e16ed79a8
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-11-09 03:14:36 +0900

    refactor: operator- 수정

--- circle4/cpp_module_00~04/cpp02/ex00/Fixed.hpp
+++ circle4/cpp_module_00~04/cpp02/ex00/Fixed.hpp
@@ -11,7 +11,7 @@ class Fixed{
     public:
         Fixed();
         Fixed(const Fixed& fixed);
-        Fixed& operator = (const Fixed& other);
+        Fixed& operator=(const Fixed& other);
         ~Fixed();
         int getRawBits(void) const;
         void setRawBits(int const raw);
